daed6657fd2a01a79ab4ab5f9ebe0aadec2ee330
Schleckenmiester <s.williamengel@gmail.com>
2022-01-16 19:53:34 -0600

    fixed hovering

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,7 @@ var app = new Vue({
 	el : '#app',
 	data : {
 		menu : {
+			isHovering : false,
 			isOpen : false
 		}
 	},
@@ -9,6 +10,10 @@ var app = new Vue({
 		toggleMenu : function() {
 			this.$refs.ham.classList.toggle('open')
 			this.menu.isOpen = !this.menu.isOpen
+		},
+		toggleMenuHover : function() {
+			this.$refs.ham.classList.toggle('hovering')
+			this.menu.isHovering = !this.menu.isHovering
 		}
 	}
 });
