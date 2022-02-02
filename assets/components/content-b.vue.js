05b5f1eb00ef4b38a8f2ddec2b84a5f0453042ea
wengel.eth <s.williamengel@gmail.com>
2022-02-02 16:49:49 -0600

    further modularized content

---
+++ assets/components/content-b.vue.js
@@ -0,0 +1,21 @@
+const ContentB = Vue.component('content-b', {
+	props: ['color', 'image', 'heading', 'p', 'text', 'link'],
+	template: `
+		<section class="content b" v-bind:class="color">
+			<div class="b" v-bind:class="color">
+				<div class="image-placeholder overlay"></div>
+				<div class="container">
+					<span class="cider"></span>
+					<h2>{{heading}}</h2>
+					<p>{{p}}</p>
+					<svg class="link-button" viewBox="0 0 250 70">
+						<router-link v-bind:to="link">
+							<polygon points="6,6 6,64 244,64 244,34 204,6"/>
+							<text x="5" y="62" text-anchor="left">{{text}}</text>
+						</router-link>
+					</svg>
+				</div>
+			</div>
+		</section>
+	`
+})
--- pages/home.vue.js
+++ pages/home.vue.js
@@ -1,28 +1,39 @@
 const Home = Vue.component('home', {
 	template: `
 		<container class="page-body">
-			<section class="title-slide top left">
-				<h1>This is a heading.</h1>
+			<section class="title-slide home top left">
+				<h1>William Engel.</h1>
 			</section>
 			<section class="content a dark-blue">
 				<div class="container">
 					<span class="cider"></span>
-					<h2>This is another heading.</h2>
+					<h2>I design brands.</h2>
 					<p>Here is some minor filler text to make is seem like there’s actually important stuff here.</p>
 				</div>
 				<div class="image-placeholder"></div>
 			</section>
-			<section class="content b dark-blue">
-				<div class="b">
-					<div class="image-placeholder overlay"></div>
-					<div class="container">
-						<span class="cider"></span>
-						<h2>This is another heading.</h2>
-						<p>Here is some minor filler text to make is seem like there’s actually important stuff here.</p>
-						<slash-button link="/" text="Button."></slash-button>
-					</div>
+			<section class="content a dark-blue">
+				<div class="container">
+					<span class="cider"></span>
+					<h2>I design take photos.</h2>
+					<p>Here is some minor filler text to make is seem like there’s actually important stuff here.</p>
 				</div>
+				<div class="image-placeholder"></div>
 			</section>
+			<section class="content a dark-blue">
+				<div class="container">
+					<span class="cider"></span>
+					<h2>I design make games.</h2>
+					<p>Here is some minor filler text to make is seem like there’s actually important stuff here.</p>
+				</div>
+				<div class="image-placeholder"></div>
+			</section>
+			<content-b color="dark-blue"
+				heading="Snippet"
+				p="Here is some minor filler text to make is seem like there’s actually important stuff here."
+				link="/"
+				text="Button."
+			></content-b>
 		</container>
 	`
 });
