182724c307a6dd0dcab95393ab26f3095c12fcbc
wengel.eth <s.williamengel@gmail.com>
2022-02-03 17:00:55 -0600

    Update projects.vue.js

--- pages/projects.vue.js
+++ pages/projects.vue.js
@@ -1,7 +1,9 @@
 const Projects = Vue.component('projects', {
 	template: `
 		<container class="page-body">
-
+			<section class="title-slide bottom left">
+				<h1>Projects.</h1>
+			</section>
 		</container>
 	`
 })
