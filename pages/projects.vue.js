938ec2cc22f2ac6c96a1a4322eab9388936f13bc
wengel.eth <s.williamengel@gmail.com>
2022-02-03 17:04:09 -0600

    added project snippets

--- pages/projects.vue.js
+++ pages/projects.vue.js
@@ -4,6 +4,34 @@ const Projects = Vue.component('projects', {
 			<section class="title-slide bottom left">
 				<h1>Projects.</h1>
 			</section>
+			<content-b color="dark-blue"
+				image=""
+				heading="Portfolio."
+				p="The website you are viewing right now. I made it. All of it."
+				link="/"
+				text="View."
+			></content-b>
+			<content-b color="dark-blue"
+				image=""
+				heading="Portfolio."
+				p="The website you are viewing right now. I made it. All of it."
+				link="/"
+				text="View."
+			></content-b>
+			<content-b color="dark-blue"
+				image=""
+				heading="Portfolio."
+				p="The website you are viewing right now. I made it. All of it."
+				link="/"
+				text="View."
+			></content-b>
+			<content-b color="dark-blue"
+				image=""
+				heading="Portfolio."
+				p="The website you are viewing right now. I made it. All of it."
+				link="/"
+				text="View."
+			></content-b>
 		</container>
 	`
 })
