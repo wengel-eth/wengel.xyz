18620f5d7a0b1a5c4c156c29a6370880bd45b606
wengel.eth <s.williamengel@gmail.com>
2022-02-04 09:49:22 -0600

    added some filler text

--- assets/components/button.vue.js
+++ assets/components/button.vue.js
@@ -4,7 +4,7 @@ const SlashButton = Vue.component('slash-button', {
 		<svg class="link-button" viewBox="0 0 250 70">
 			<router-link v-bind:to="link">
 				<polygon points="6,6 6,64 244,64 244,34 204,6"/>
-				<text x="5" y="62" text-anchor="left">{{text}}</text>
+				<text x="9" y="61" text-anchor="left">&nbsp;{{text}}</text>
 			</router-link>
 		</svg>
 	`
