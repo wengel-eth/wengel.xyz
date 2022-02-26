067d2ef976dbf7365dc6c630afee799a55593092
Schleckenmiester <s.williamengel@gmail.com>
2022-02-25 22:34:40 -0600

    changed image host.

--- pages/home.vue.js
+++ pages/home.vue.js
@@ -2,21 +2,21 @@ const Home = Vue.component('home', {
 	template: `
 		<container class="page-body">
 			<section class="title-slide home top left"
-				style="background-image:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.2)),url('https://wengel-xyz-images.nyc3.cdn.digitaloceanspaces.com/IMG_7325.jpg');">
+				style="background-image:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.2)),url('https://ik.imagekit.io/wengel/wengel_xyz/IMG_7325_lm1xxNeU8c6t.jpg');">
 				<h1>William Engel.</h1>
 			</section>
 			<content-a color="dark-blue"
-				image="https://wengel-xyz-images.nyc3.cdn.digitaloceanspaces.com/engelheim_website.jpg"
+				image="https://ik.imagekit.io/wengel/wengel_xyz/engelheim_website_TYgHCU5MitL.jpg"
 				heading="I design brands."
 				p="Here is some minor filler text to make is seem like there’s actually important stuff here."
 			></content-a>
 			<content-a color="dark-blue"
-				image="https://wengel-xyz-images.nyc3.cdn.digitaloceanspaces.com/IMG_9820.jpg"
+				image="https://ik.imagekit.io/wengel/wengel_xyz/IMG_9820_1QFj9qXS2-x.jpg"
 				heading="I capture photos."
 				p="Here is some minor filler text to make is seem like there’s actually important stuff here."
 			></content-a>
 			<content-a color="dark-blue"
-				image="https://wengel-xyz-images.nyc3.cdn.digitaloceanspaces.com/IMG_0250.jpg"
+				image="https://ik.imagekit.io/wengel/wengel_xyz/IMG_0250_ATRfb6sMQcXB.jpg"
 				heading="I make games."
 				p="Here is some minor filler text to make is seem like there’s actually important stuff here."
 			></content-a>
--- pages/projects.vue.js
+++ pages/projects.vue.js
@@ -1,11 +1,11 @@
 const Projects = Vue.component('projects', {
 	template: `
 		<container class="page-body">
-			<section class="title-slide bottom left" style="background-image:linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.5)),url('https://wengel-xyz-images.nyc3.cdn.digitaloceanspaces.com/IMG_9655.jpg')">
+			<section class="title-slide bottom left" style="background-image:linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.5)),url('https://ik.imagekit.io/wengel/wengel_xyz/IMG_9655_z_yUF3J6VC3.jpg')">
 				<h1>Projects.</h1>
 			</section>
 			<content-b color="dark-blue"
-				image="background-image:url('https://wengel-xyz-images.nyc3.cdn.digitaloceanspaces.com/RED.jpg');"
+				image="background-image:url('https://ik.imagekit.io/wengel/wengel_xyz/RED_bGcwi3mkYuSJ.jpg');"
 				heading="Website."
 				p="The website you are viewing right now. Handcrafted with Hypertext \
 					Markup Language, styled with Cascading Stylesheets, and structured \
