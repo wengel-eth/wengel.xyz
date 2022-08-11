336d09442005d9e780a00ef9de5816715b30aaea
wengel.eth <s.williamengel@gmail.com>
2022-08-11 16:54:25 -0600

    added progressive delivery.

--- pages/home.vue.js
+++ pages/home.vue.js
@@ -2,23 +2,23 @@ const Home = Vue.component('home', {
 	template: `
 		<container class="page-body">
 			<section class="title-slide home top left"
-				style="background-image:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.2)),url('https://ik.imagekit.io/wengel/wengel_xyz/IMG_7325_lm1xxNeU8c6t.jpg?tr=w-1440');">
+				style="background-image:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.2)),url('https://ik.imagekit.io/wengel/wengel_xyz/IMG_7325_lm1xxNeU8c6t.jpg?tr=w-1440,pr-true');">
 				<h1>William Engel.</h1>
 			</section>
 			<content-a color="dark-blue"
-				image="https://ik.imagekit.io/wengel/wengel_xyz/engelheim_website_TYgHCU5MitL.jpg?tr=w-1440"
+				image="https://ik.imagekit.io/wengel/wengel_xyz/engelheim_website_TYgHCU5MitL.jpg?tr=w-1440,pr-true"
 				link="/"
 				heading="I create brands."
 				p="Here is some minor filler text to make is seem like there’s actually important stuff here."
 			></content-a>
 			<content-a color="dark-blue"
-				image="https://ik.imagekit.io/wengel/wengel_xyz/IMG_9820_1QFj9qXS2-x.jpg?tr=w-1440"
+				image="https://ik.imagekit.io/wengel/wengel_xyz/IMG_9820_1QFj9qXS2-x.jpg?tr=w-1440,pr-true"
 				link="/"
 				heading="I capture photos."
 				p="Here is some minor filler text to make is seem like there’s actually important stuff here."
 			></content-a>
 			<content-a color="dark-blue"
-				image="https://ik.imagekit.io/wengel/wengel_xyz/IMG_0250_ATRfb6sMQcXB.jpg?tr=w-1440"
+				image="https://ik.imagekit.io/wengel/wengel_xyz/IMG_0250_ATRfb6sMQcXB.jpg?tr=w-1440,pr-true"
 				link="/"
 				heading="I construct games."
 				p="Here is some minor filler text to make is seem like there’s actually important stuff here."
