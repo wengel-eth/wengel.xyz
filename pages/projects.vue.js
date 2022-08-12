c00e378415fa86ccd61ea404e6a650b20137aa80
wengel.eth <s.williamengel@gmail.com>
2022-08-12 13:54:24 -0600

    added molllo website image.

--- pages/projects.vue.js
+++ pages/projects.vue.js
@@ -15,7 +15,7 @@ const Projects = Vue.component('projects', {
 				text="View."
 			></content-b>
 			<content-b color="dark-blue"
-				image="background-image:url('https://ik.imagekit.io/wengel/wengel_xyz/engelheim_website_TYgHCU5MitL.jpg?tr=w-720,h-720,fo-center,pr-true');"
+				image="background-image:url('https://ik.imagekit.io/wengel/wengel_xyz/Perspective-Web-Mockup-17-Main_File_3Fb72KHy1.png?tr=w-720,h-720,fo-center,pr-true');"
 				heading="molllo.com"
 				p="Another handcrafted masterpiece. This elegant blog is curated with \
 					excellent articles aimed at providing exceptional assets from across \
