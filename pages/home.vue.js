eb8849f721c59971833ec18f6da01e960e31a7d1
wengel.eth <54996189+wengel-eth@users.noreply.github.com>
2022-08-12 20:48:16 -0600

    upped image sizes.

--- pages/home.vue.js
+++ pages/home.vue.js
@@ -2,23 +2,23 @@ const Home = Vue.component('home', {
 	template: `
 		<container class="page-body">
 			<section class="title-slide home top left"
-				style="background-image:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.2)),url('https://ik.imagekit.io/wengel/wengel_xyz/IMG_7325_lm1xxNeU8c6t.jpg?tr=w-1080,pr-true');">
+				style="background-image:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.2)),url('https://ik.imagekit.io/wengel/wengel_xyz/IMG_7325_lm1xxNeU8c6t.jpg?tr=w-1440,pr-true');">
 				<h1>William Engel.</h1>
 			</section>
 			<content-a color="dark-blue"
-				image="https://ik.imagekit.io/wengel/wengel_xyz/twitter-mockup_I3xIzXzBM.jpg?tr=w-1080,pr-true"
+				image="https://ik.imagekit.io/wengel/wengel_xyz/twitter-mockup_I3xIzXzBM.jpg?tr=w-1440,pr-true"
 				link="https://twitter.com/wengel_eth"
 				heading="I tweet truth."
 				p="The world needs more people willing to tell the truth, not less. Whether the entire world is against it or not, my tweets mainly compromise with Biblical takes on contemporary issues. “All flesh is like grass, and all its glory like the flower of grass. The grass withers, and the flower falls off, but the word of the Lord endures forever.” (1 Peter 1:24-25a)"
 			></content-a>
 			<content-a color="dark-blue"
-				image="https://ik.imagekit.io/wengel/wengel_xyz/IMG_9820_1QFj9qXS2-x.jpg?tr=w-1080,pr-true"
+				image="https://ik.imagekit.io/wengel/wengel_xyz/IMG_9820_1QFj9qXS2-x.jpg?tr=w-1440,pr-true"
 				link="/"
 				heading="I capture photos."
 				p="Here is some minor filler text to make is seem like there’s actually important stuff here."
 			></content-a>
 			<content-a color="dark-blue"
-				image="https://ik.imagekit.io/wengel/wengel_xyz/IMG_0250_ATRfb6sMQcXB.jpg?tr=w-1080,pr-true"
+				image="https://ik.imagekit.io/wengel/wengel_xyz/IMG_0250_ATRfb6sMQcXB.jpg?tr=w-1440,pr-true"
 				link="/"
 				heading="I construct games."
 				p="Here is some minor filler text to make is seem like there’s actually important stuff here."
--- pages/projects.vue.js
+++ pages/projects.vue.js
@@ -1,11 +1,11 @@
 const Projects = Vue.component('projects', {
 	template: `
 		<container class="page-body">
-			<section class="title-slide bottom left" style="background-image:linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.5)),url('https://ik.imagekit.io/wengel/wengel_xyz/IMG_9655_z_yUF3J6VC3.jpg?tr=w-1080,pr-true')">
+			<section class="title-slide bottom left" style="background-image:linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.5)),url('https://ik.imagekit.io/wengel/wengel_xyz/IMG_9655_z_yUF3J6VC3.jpg?tr=w-1440,pr-true')">
 				<h1>Projects.</h1>
 			</section>
 			<content-b color="dark-blue"
-				image="background-image:url('https://ik.imagekit.io/wengel/wengel_xyz/engelheim_website_TYgHCU5MitL.jpg?tr=w-720,h-720,fo-center,pr-true');"
+				image="background-image:url('https://ik.imagekit.io/wengel/wengel_xyz/engelheim_website_TYgHCU5MitL.jpg?tr=w-1080,h-1080,fo-center,pr-true');"
 				heading="wengel.xyz"
 				p="The website you are viewing right now. Handcrafted with Hypertext \
 					Markup Language, styled with Cascading Stylesheets, and structured \
@@ -15,7 +15,7 @@ const Projects = Vue.component('projects', {
 				text="View."
 			></content-b>
 			<content-b color="dark-blue"
-				image="background-image:url('https://ik.imagekit.io/wengel/wengel_xyz/Perspective-Web-Mockup-17-Main_File_3Fb72KHy1.png?tr=w-720,h-720,fo-center,pr-true');"
+				image="background-image:url('https://ik.imagekit.io/wengel/wengel_xyz/Perspective-Web-Mockup-17-Main_File_3Fb72KHy1.png?tr=w-1080,h-1080,fo-center,pr-true');"
 				heading="molllo.com"
 				p="Another handcrafted masterpiece. This elegant blog is curated with \
 					excellent articles aimed at providing exceptional assets from across \
